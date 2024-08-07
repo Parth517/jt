52afa66cc17d8da8690e87e02fc8a73bfbad4aca
Parth Deshpande <parthjun27@gmail.com>
2024-08-07 20:35:48 +0530

    implement:Write test condition

--- jtest/src/test/java/com/example/NumberIsPrimeTest.java
+++ jtest/src/test/java/com/example/NumberIsPrimeTest.java
@@ -3,5 +3,29 @@ package com.example;
 import org.junit.jupiter.api.BeforeEach;
 
 public class NumberIsPrimeTest {
- 
+    private NumberIsPrime np;
+    private static int passedTests = 0;
+    private static int failedTests = 0;
+
+    @BeforeEach
+    public void setup() {
+        np = new NumberIsPrime();
+    }
+
+    public void tearDown() {
+        System.out.println("No of tests passed:" + passedTests);
+        System.out.println("No of failed passed:" + failedTests);
+    }
+
+    public void runTest(int number, boolean expectedResult) {
+        int result = isPrime(number);
+        if (result == expectedResult) {
+            passedTests++;
+            System.out.println("Test passed for " + input + " expected result " + expectedResult + " got " + result);
+        } else {
+            failedTests++;
+            System.out.println("Test failed for " + input + " expected result " + expectedResult + " got " + result);
+        }
+    }
+
 }
