7eb82036eedf6d79c76095a68b7e019219bebd75
Parth Deshpande <parthjun27@gmail.com>
2024-08-07 19:45:55 +0530

    test:writing test cases

---
+++ jtest/src/test/java/com/example/DuplicateCharTest.java
@@ -0,0 +1,40 @@
+package com.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DuplicateCharTest {
+    private DuplicateChar dc;
+    private static int passedTests=0;
+    private static int failedTests=0;
+
+    @BeforeEach
+    public void setup(){
+        dc=new DuplicateChar();
+    }
+
+    @AfterAll
+    public void tearDown(){
+        System.out.println("No of tests passed: " + passedTests);
+        System.out.println("No of tests failed: " + failedTests);
+    }
+
+     private void runTest(String input, boolean expectedResult) {
+        boolean result = dc.isDuplicate(input);
+        if (result == expectedResult) {
+            passedTests++;
+            System.out.println("Test passed for " + input + " expected result " + expectedResult + " got " + result);
+        } else {
+            failedTests++;
+            System.out.println("Test failed for " + input + " expected result " + expectedResult + " got " + result);
+        }
+        assertEquals(expectedResult, result);
+    }
+    @Test
+    public void testCheckDuplicateIn2Chars() {
+        runTest("AA", true);
+    }
+}
