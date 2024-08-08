684694eeeea6d952ecc4bb8d7f8ddf7f177efc36
Parth Deshpande <parthjun27@gmail.com>
2024-08-08 21:35:52 +0530

    Initializing the project strucutre

---
+++ jtest/src/test/java/com/example/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class StringCalculatorTest {
+    private StringCalculator stringCalculator;
+    private static int passedTests=0;
+    private static int failedTests=0;
+
+}
