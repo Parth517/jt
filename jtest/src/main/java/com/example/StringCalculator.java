eefd1b2e66e48885ace14445fe9b3c446192a1fb
Parth Deshpande <parthjun27@gmail.com>
2024-08-08 22:11:09 +0530

    passes test when 1 number is passed to the function

--- jtest/src/main/java/com/example/StringCalculator.java
+++ jtest/src/main/java/com/example/StringCalculator.java
@@ -1,11 +1,14 @@
 package com.example;
 
 public class StringCalculator {
-    public static int add(String number){
-        int ans=0;
-        number=number.replaceAll(" ","");
-        if(number=="") ans=0;
-        return ans;
+    public static int add(String number) {
+        int ans = 0;
+        number = number.replaceAll(" ", "");
+        if (number.equals("")) {
+            return 0;
+        }else{
+            return Integer.parseInt(number);
+        }
     }
 
 }
