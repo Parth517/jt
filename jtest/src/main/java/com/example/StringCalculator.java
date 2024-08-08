490ecac528a77c45152c92e04b8d7e9317bd7787
Parth Deshpande <parthjun27@gmail.com>
2024-08-08 21:44:38 +0530

    passing test when empty string is passed

---
+++ jtest/src/main/java/com/example/StringCalculator.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class StringCalculator {
+    public static int add(String number){
+        int ans=0;
+        if(number==" "|| number=="") ans=0;
+        return ans;
+    }
+
+}
