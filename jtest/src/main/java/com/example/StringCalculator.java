4731c0a5f641ea169becc87d28fa0b68af880573
Parth Deshpande <parthjun27@gmail.com>
2024-08-08 22:44:34 +0530

    refactor the fucntion to enusre addition of multiple numbers passed in String

--- jtest/src/main/java/com/example/StringCalculator.java
+++ jtest/src/main/java/com/example/StringCalculator.java
@@ -6,21 +6,22 @@ public class StringCalculator {
         number = number.replaceAll(" ", "");
         if (number.equals("")) {
             return 0;
-        }else if(number.contains(",")){
-            String[] stringOfNumbers=number.split(",");
-            int sum=0;
-            for(int i=0;i<stringOfNumbers.length;i++){
-                sum+=Integer.parseInt(stringOfNumbers[i]);
-            }
-            return sum;
-        }
-        else{
-            return Integer.parseInt(number);
+        } else{
+            return sumOfNumber(number);
         }
     }
 
     private static int toInt(String number) throws NumberFormatException {
         return Integer.parseInt(number);
+    }
+
+    private static int sumOfNumber(String number) throws NumberFormatException {
+        String[] stringOfNumbers = number.split(",");
+        int sum = 0;
+        for (String num : stringOfNumbers) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
     }
 
 }
